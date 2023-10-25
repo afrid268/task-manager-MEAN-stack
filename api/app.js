a9265fed9537d44459a75a20470b7829dd69240d
afrid268 <79365878+afrid268@users.noreply.github.com>
2023-10-25 12:13:57 +0530

    updated commit
    connection front end and backend , cors implemented , created webservices , created task services , new list in task html clicked to save a list

--- api/app.js
+++ api/app.js
@@ -12,6 +12,13 @@ const { List,Task } = require('./db/models');
 
 app.use(bodyParser.json());
 
+//cors middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 /*List Routes*/
 app.get('/lists',(req,res) => {
     List.find({}).then((lists)=>{
